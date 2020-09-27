f22aaafae0883e6b38bdc7e797a7f80b7911380d
Bmoser <barbaramoser91@gmail.com>
2020-09-27 11:33:09 -0300

    criando estrutura para o projeto

---
+++ src/components/FormPessoas.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <form v-if="pessoas.length != qtdePessoas" v-on:submit.prevent = "adicionarPessoa()">
+      <label>Nome</label>
+      <input type="text" placeholder="Informe seu nome" required v-model="novaPessoa.nome"/>
+      <label>Email</label>
+      <input type="email" placeholder="Informe seu email" required v-model="novaPessoa.email"/>   
+      <br>
+      <button class="button" type="submit">Adicionar</button>  
+    </form>
+    <p v-else>A quantidade de pessoas foi atingida!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pessoas: Array,
+    qtdePessoas: Number,
+  },
+  data() {
+    return {
+      novaPessoa: {
+        nome: '',
+        email: '',
+      }
+    }
+  },
+  methods: {
+    adicionarPessoa() {
+      this.$emit('adicionar', this.novaPessoa)
+    }
+  }
+}
+</script>
+
+<style>
+p {
+  color: orangered;
+  font-size: 20px;
+  font: bolder;
+}
+
+</style>
---
+++ src/components/TablePessoas.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+        <table class="tableSorteio">
+            <tr>
+                <th>
+                    Nome
+                    <img src="../images/asc.png" width="20px" height="20px" @click="ordenarPessoa('asc')">
+                    <img src="../images/desc.png" width="20px" height="20px" @click="ordenarPessoa('desc')">
+                </th>
+                <th>Email</th> 
+                <th>Ações</th>      
+            </tr>
+            <tr class='tabelaPessoa' v-for="(p,index) in pessoas" :key="index">
+                <td id='nome' :class="{'destacar': pessoaSorteada(p.nome)}">{{p.nome}}</td>
+                <td id='email' :class="{'destacar': pessoaSorteada(p.nome)}">{{p.email}}</td>
+                <td>
+                    <img class="botaoImagem" src="../images/delete.png" width="20px" height="20px" @click="delPessoa(index)">
+                    <img class="botaoImagem" src="../images/alterar.png" width="20px" height="20px" @click="alterarPessoa(index)">
+                </td>
+            </tr>
+        </table>
+    </div>    
+</template>
+
+<script>
+export default {
+    props: {
+        pessoas: Array,
+        nomeSorteado: String,
+    },
+    methods: {
+        pessoaSorteada(nome) {
+            return this.nomeSorteado === nome
+        },
+        delPessoa(index) {
+            console.log('Entrou no método deletaPessoa')
+            this.$emit('deletar', index)
+        },
+        ordenarPessoa(ordem) {
+            console.log('Entrou no método ordenarPessoa')
+            this.$emit('ordenar', ordem)
+        },
+        alterarPessoa(index) {
+            console.log('Entrou no método alterarPessoa')
+            this.$emit('alterar', index)
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+.destacar {
+    background-color: lightblue;
+}
+
+.botaoImagem {
+    padding: 5px;
+}
+
+table, th, td {
+  border: 3px solid black;
+  border-collapse: collapse;
+  position: f;
+}
+
+td {
+    margin-left: 1px;
+}
+
+table {
+  width: 40%;
+  margin-left: 30%;
+  margin-top: 1%;
+}
+
+</style>
