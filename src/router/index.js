896dbc04b15c9dbc210011bd451fab0d661e9b70
Bmoser <barbaramoser91@gmail.com>
2020-10-16 23:20:26 -0300

    criando rotas para dashboard e projeto, alem dos calculos solicitados na prova

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Order from '../views/Order.vue'
-import Brands from '../views/Brands.vue'
-import NewBrand from '../views/NewBrand.vue'
+import Projetos from '../views/Projetos.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 Vue.use(VueRouter)
 
@@ -14,20 +13,15 @@ const routes = [
         component: Home
     },
     {
-        path: '/order',
-        name: 'Order',
-        component: Order
+        path: '/projetos',
+        name: 'Projetos',
+        component: Projetos
     },
     {
-        path: '/brands',
-        name: 'Brands',
-        component: Brands
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: Dashboard
     },
-    {
-        path: '/brands/new',
-        name: 'NewBrand',
-        component: NewBrand
-    }
 ]
 
 const router = new VueRouter({
